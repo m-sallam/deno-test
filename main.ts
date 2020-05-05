cc7e06c840b81934174cf4912c2d9069ccfc94d8
m-sallam <36113254+m-sallam@users.noreply.github.com>
2020-05-06 01:23:12 +0200

    updaated main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
-import { Application } from "https://deno.land/x/espresso/mod.ts";
-const app = new Application();
+import { serve } from "https://deno.land/std@v1.0.0-rc1/http/server.ts";
+import * as flags from "https://deno.land/std@v1.0.0-rc1/flags/mod.ts";
 
-const { PORT=3000 } = Deno.env()
+const DEFAULT_PORT = 8080;
+const argPort = flags.parse(Deno.args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
-app.get("/", context => {
-  context.send("Hello From Deno !");
-});
-app.start(PORT)
+if (isNaN(port)) {
+  console.error("Port is not a number.");
+  Deno.exit(1);
+}
+
+const body = new TextEncoder().encode("Hello World\n");
+const s = serve({ port: port, hostname: '0.0.0.0'});
+console.log("http://localhost:" + port);
+
+for await (const req of s) {
+  req.respond({ body });
+}
