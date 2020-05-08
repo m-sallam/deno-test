78bd49bf860e01d8513798cb25943e06deebbdf2
m-sallam <muhammad.sallam@outlook.com>
2020-05-08 13:02:03 +0200

    whatever

--- api/test-lambda.js
+++ api/test-lambda.js
@@ -2,9 +2,9 @@ export async function handler(
   event,
   context
 ){
-  console.log(event.body?.body)
-  const b = new Deno.Buffer()
-  console.log(b.readSync(event.body?.body))
+  if (event.body?.body) {
+    console.log(JSON.parse(atob(event.body?.body)));
+  }
   return {
     body: `Welcome to deno ${Deno.version.deno} 🦕`,
     headers: { "content-type": "text/html;charset=utf8" },
